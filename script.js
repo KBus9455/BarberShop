38cf2377a53ad668bca4d70ef2eff01112df5b37
Katarzyna Bus <Katarzyna.Bus9455@gmail.com>
2021-03-25 22:14:08 +0100

    add slider in producta

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const btnMenu = document.querySelector("i");
 const listMenu = document.querySelector("nav");
 
+let x = 0; 
 
 btnMenu.addEventListener("click", function (){
     listMenu.classList.toggle("active");
 })
 
 
+const products =document.querySelector('.products');
+const productsTab = ['fotos/s3.jpg', 'fotos/s2.jpg', 'fotos/s1.jpg'];
+products.src = productsTab[x];
 
 
- $('.aboutUs').on('click', function () {
-  $('body, html').animate({
-   scrollTop: $('.aboutUs').offset().top
-  }, 500)
- })
+
+function change() {
+    x++;
+    if (x == 3) {
+        x = 0;
+    }
+    products.src = productsTab[x];
+}
+setInterval(change, 4000);
+
+
+
+
+//
+ //$('.aboutUs').on('click', function () {
+ // $('body, html').animate({
+//   scrollTop: $('.aboutUs').offset().top
+//  }, 500)
+// }) 
